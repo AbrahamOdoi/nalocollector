7f377ca2344c5154dcc0786b91c5030e4980210f
AbrahamOdoi <abrahamodoi2002@gmail.com>
2015-01-02 14:19:02 +0000

    2nd Jan
    first

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).on("pagecreate", function() {
+	$("#splahLabel1").slideDown(3000, function() {
+		$("#splahLabel2").fadeIn(3000, function() {
+			window.location = 'login.php';
+		});
+	});
+});
---
+++ loginval.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once 'core/connection.php';
+$user = $_POST['username'];
+$pass = $_POST['pass'];
+
+
+$execute = mysqli_query($con, "select * from users where username='$user' and password='$pass'");
+if (mysqli_num_rows($execute) > 0) {
+	echo '1';
+} else {
+	echo "0";
+}
+?>
