ed50ceba31af4e47cd71a9f2b3ba2a7afb882c5f
AbrahamOdoi <abrahamodoi2002@gmail.com>
2015-01-02 15:09:02 +0000

    third

--- loginval.php
+++ loginval.php
@@ -5,10 +5,10 @@ $user = $_POST['username'];
 $pass = $_POST['pass'];
 
 
-$execute = mysqli_query($con, "select * from users where username='$user' and password='$pass'");
-if (mysqli_num_rows($execute) > 0) {
-	echo '1';
-} else {
+$execute = mysqli_query($con, "select * from users where username='$user' and password='$pass' limit 1");
+if (mysqli_num_rows($execute) != 1) {
 	echo "0";
+} else {
+	echo '1';
 }
 ?>
